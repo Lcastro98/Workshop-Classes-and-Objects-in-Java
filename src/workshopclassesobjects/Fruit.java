cc0eb00d9bcb0bd38d79201b64f1b66248bfd62e
Lorena Castro <lcastro0398@gmail.com>
2022-02-04 09:20:08 -0500

    Se agregan los atributos, constructor y métodos de la clase Fruit.

--- src/workshopclassesobjects/Fruit.java
+++ src/workshopclassesobjects/Fruit.java
@@ -3,11 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package workshopclassesobjects;
-
+import java.util.ArrayList;
 /**
  *
- * @author lcast
+ * @author Lorena Castro
  */
 public class Fruit {
+    public String name;
+    private float averageWeight;
+    public ArrayList<String> colors;
+
+    /**
+     * Constructor: Crea una instancia del objeto Fruit
+     * @param name
+     * @param averageWeight
+     * @param colors 
+     */
+ 
+    public Fruit(String name, float averageWeight, ArrayList<String> colors) {
+        this.name = name;
+        this.averageWeight = averageWeight;
+        this.colors = colors;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+
+    /**
+     * 
+     * @param colors 
+     */
+    public void setColors(ArrayList<String> colors) {
+        this.colors = colors;
+    }
     
 }
