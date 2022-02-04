a096bc6ab239a619e8ec6a7b3df650873e4a760b
Lorena Castro <lcastro0398@gmail.com>
2022-02-04 09:38:36 -0500

    Se agregan los atributos, constructor y métodos de la clase BankAccount.

--- src/workshopclassesobjects/BankAccount.java
+++ src/workshopclassesobjects/BankAccount.java
@@ -6,8 +6,36 @@ package workshopclassesobjects;
 
 /**
  *
- * @author lcast
+ * @author Lorena Castro
  */
 public class BankAccount {
+    private int accountNumber;
+    protected boolean activated;
+
+    /**
+     * Constructor: crea una instancia de la clase BankAccount.
+     * @param accountNumber
+     * @param activated 
+     */
+    public BankAccount(int accountNumber, boolean activated) {
+        this.accountNumber = accountNumber;
+        this.activated = activated;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public boolean isActivated() {
+        return activated;
+    }
+
+    /**
+     * 
+     * @param activated 
+     */
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
     
 }
