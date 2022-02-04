e7fda0956a1667e83c7f156bc5219d7f173b4202
Lorena Castro <lcastro0398@gmail.com>
2022-02-04 10:51:05 -0500

    Se agregan los atributos, constructor y métodos de la clase Product.

--- src/workshopclassesobjects/Product.java
+++ src/workshopclassesobjects/Product.java
@@ -6,8 +6,75 @@ package workshopclassesobjects;
 
 /**
  *
- * @author lcast
+ * @author Lorena Castro
  */
 public class Product {
+    protected int UPC;
+    public String name;
+    public String description;
+    private long price;
+
+    /**
+     * Constructor: crea una instancia de la clase Product.
+     * @param UPC
+     * @param name
+     * @param description
+     * @param price 
+     */
+    public Product(int UPC, String name, String description, long price) {
+        this.UPC = UPC;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name 
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * 
+     * @param description 
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public long getPrice() {
+        return price;
+    }
+
+    /**
+     * 
+     * @param price 
+     */
+    public void setPrice(long price) {
+        this.price = price;
+    }
+    
     
 }
