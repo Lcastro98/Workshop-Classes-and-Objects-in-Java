5009709d5fcbf7c31009aed2fde2787e998973d5
Lorena Castro <lcastro0398@gmail.com>
2022-02-04 11:02:16 -0500

    Se agregan los atributos, constructor y métodos de la clase Smartphone.

--- src/workshopclassesobjects/Smartphone.java
+++ src/workshopclassesobjects/Smartphone.java
@@ -6,8 +6,48 @@ package workshopclassesobjects;
 
 /**
  *
- * @author lcast
+ * @author Lorena Castro
  */
 public class Smartphone {
+    protected String marca;
+    protected String modelo;
+    public String sistemaOperativo;
+    public String procesador;
+    public String RAM;
+    public String memoria;
+
+    /**
+     * Constructor: crea una instancia de la clase Smartphone.
+     * @param marca
+     * @param modelo
+     * @param sistemaOperativo
+     * @param procesador
+     * @param RAM
+     * @param memoria 
+     */
+    public Smartphone(String marca, String modelo, String sistemaOperativo, String procesador, String RAM, String memoria) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.sistemaOperativo = sistemaOperativo;
+        this.procesador = procesador;
+        this.RAM = RAM;
+        this.memoria = memoria;
+    }
+
+    /**
+     * Método accesor del atributo Marca.
+     * @return 
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * Método modificador del atributo Marca.
+     * @param marca 
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
     
 }
